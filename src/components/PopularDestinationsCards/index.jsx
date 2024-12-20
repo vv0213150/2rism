2206af4591be7f5d4cd37201ef890e3ae6ff9bd5
Пользователь <vv0213159@gmail.com>
2024-12-20 22:49:48 +0600

    Добавлены alt атрибуты для изображений в PopularDestinationsCards и TravelTips

--- src/components/PopularDestinationsCards/index.jsx
+++ src/components/PopularDestinationsCards/index.jsx
@@ -14,7 +14,7 @@ const PopularDestinationsCard = (props) => {
               <div className={cls.gradeTwo}>
                 {props.icon} 
                 <div className={cls.text}><p>{props.p_text}</p></div>
-                <div><img src={props.images}/></div>
+                <div><img src={props.images} alt="img"/></div>
               </div>
             </div>
         </div>
